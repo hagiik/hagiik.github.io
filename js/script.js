32470797e08faa44142818e171d63c3a9c3f5855
MuhamadAzizi <muhamadazizi177@yahoo.com>
2021-08-18 07:53:14 +0700

    Fixed

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
